14bf8ecb2b0d4d2802d83de8aa1e822b5c0f0bdc
tatsu0529 <tatsutatsuyan0529@gmail.com>
2019-12-31 05:51:02 +0000

    D6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+  
+  def edit
+  end
+  
+  def update
+      @user = User.find(params[:id])
+    if @user.update_atrributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_url
+    else
+      render :edit_user_url
+    end
+  end
 
   def create
     @user = User.new(user_params)
